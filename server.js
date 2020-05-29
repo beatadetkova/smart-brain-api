3460ae7df6216b4a07e267ffb7c9e204feeb5a93
Beata Detkova <beata.detkova.jr@gmail.com>
2020-05-29 17:25:30 +0200

    Initial commit with local files

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+require('dotenv').config()
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : process.env.DB_HOST,
+    user : 'postgres',
+    password : process.env.DB_PWD,
+    database : process.env.DB_NAME
+  }
+});
+
+db.select('*').from('users').then(data => {
+  console.log(data);
+});
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors())
+
+app.get('/', (req, res) => { res.send(database.users) })
+app.post('/signin', signin.handleSignin(db, bcrypt))
+app.post('/register', register.handleRegister(db, bcrypt))
+app.get('/profile/:id', profile.handleProfileGet(db))
+app.put('/image', image.handleImage(db))
+app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)})
+
+app.listen(3000, ()=> {
+  console.log('app is running on port 3000')
+})

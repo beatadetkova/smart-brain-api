9c37b8bbc823e000e1f86bb9ae9c953f8831db21
Beata Detkova <beata.detkova.jr@gmail.com>
2020-06-08 17:21:30 +0200

    fix typo

--- controllers/profile.js
+++ controllers/profile.js
@@ -2,7 +2,7 @@ const handleProfileGet = (db) => (req, res) => {
   const { id } = req.params;
   db.select('*').from('users').where({id})
     .then(user => {
-    if(user.lenght) {
+    if(user.length) {
       res.json(user[0])
     } else {
       res.status(400).json('Not found')

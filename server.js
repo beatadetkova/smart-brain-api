a782c9c0b6b21a239a371ef073216393fc86eef7
Beata Detkova <beata.detkova.jr@gmail.com>
2020-06-08 16:51:56 +0200

    updating database structure

--- server.js
+++ server.js
@@ -16,9 +16,7 @@ const db = knex({
   client: 'pg',
   connection: {
     host : process.env.DB_HOST,
-    user : 'postgres',
-    password : process.env.DB_PWD,
-    database : process.env.DB_NAME
+    ssl: true,
   }
 });
 
